5c88305408efdc8efe061592d37bf0933d4d3735
d-levchenko <dl.qubb@gmail.com>
2026-04-16 21:35:14 +0300

    fetching info from countries API; list generation works

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -1,10 +1,50 @@
-// import css from './Countries.module.css';
+import { useEffect, useState } from 'react';
+
+import type { Country } from '../../types/country';
 import fetchCountries from '../../services/countryService';
+import css from './Countries.module.css';
 
 const Countries = () => {
-  console.log(fetchCountries());
+  const [countries, setCountries] = useState<Country[]>([]);
 
-  return <div>{/* <h1 className={css.container}>Countries</h1> */}</div>;
+  useEffect(() => {
+    const loadCountries = async () => {
+      const data = await fetchCountries();
+      setCountries(data);
+    };
+
+    loadCountries();
+  }, []);
+
+  return (
+    <div className={css.countryWrapper}>
+      <ul className={css.countries}>
+        {countries.map(country => (
+          <li key={country.cca3} className={css.countryItem}>
+            <img
+              className={css.image}
+              src={country.flags.png}
+              alt={country.name.common}
+            />
+            <div className={css.wrapperText}>
+              <p className={css.countryName}>
+                <strong>{country.name.common}</strong>
+              </p>
+              <p>
+                <strong>Population: </strong> {country.population}
+              </p>
+              <p>
+                <strong>Region:</strong> {country.region}
+              </p>
+              <p>
+                <strong>Capital:</strong> {country.capital?.[0] ?? 'No capital'}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Countries;
--- src/services/countryService.ts
+++ src/services/countryService.ts
@@ -1,11 +1,24 @@
 import axios from 'axios';
+import type { Country } from '../types/country';
 
-const fetchCountries = async () => {
-  const response = await axios.get(
-    'https://restcountries.com/v3.1/all?fields=name',
-  );
+const fetchCountries = async (): Promise<Country[]> => {
+  const url =
+    'https://restcountries.com/v3.1/all?fields=name,cca3,capital,population,region,subregion,flags,tld,currencies,languages';
 
-  return response.data;
+  try {
+    const response = await axios.get<Country[]>(url);
+    if (!response || !response.data) {
+      throw new Error('No data returned from API');
+    }
+    return response.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+    } else {
+      console.log('Unknown error occurred');
+    }
+    return [];
+  }
 };
 
 export default fetchCountries;
